ea5ab69e8ba5de9125c894ea537284797fd90914
Ratthapon Sananaur <poom8165@gmail.com>
2022-11-16 23:48:16 +0700

    [fix]: route

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ let cors = require('cors')
 let dotenv = require('dotenv')
 const cookieParser = require('cookie-parser')
 
-const app = express();
-dotenv.config();
+const app = express()
+dotenv.config()
 
-app.use('*', cors({
-  origin: '*',
-  methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
-}));
+app.use(
+  '*',
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
+  })
+)
 // app.use(cors());
 const bookAPI = require('./routes/book.route')
 const cartAPI = require('./routes/cart.route')
@@ -19,21 +22,23 @@ const orderAPI = require('./routes/order.route')
 const userAPI = require('./routes/user.route')
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-  extended: false
-}))
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+)
 app.use(cookieParser())
 
-app.use('/api/products', bookAPI)
-app.use('/api/carts', cartAPI)
-app.use('/api/orders', orderAPI)
-app.use('/api/user', userAPI)
+app.use('api/products', bookAPI)
+app.use('api/carts', cartAPI)
+app.use('api/orders', orderAPI)
+app.use('api/user', userAPI)
 
-const PORT = process.env.PORT || 3000;
-mongoose.connect(process.env.CONNECTION_URL,
-  {
+const PORT = process.env.PORT || 3000
+mongoose
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
   .then(() => app.listen(PORT, () => console.log(`PORT : ${PORT}`)))
-  .catch((error) => console.error(error.message));
+  .catch((error) => console.error(error.message))
